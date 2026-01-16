0b6cd7cfe15b3fb910d9213e4bc5de47f9dd0d12
sirisart <mr.sirisart1999@gmail.com>
2026-01-16 11:04:33 +0700

    68011068 test4

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,41 @@
-export function countCommentsByPost() {}
+import axios from "axios";
+
+type ApiPost = {
+  postId: number;
+  id: number;
+  name: string;
+  email:string;
+  body: string;
+};
+
+type UserPost = {
+  id: number;
+  body_counter: number = 0;
+};
+
+export async function countCommentsByPost(): Promise<UserPost[]> {
+  try {
+    const response = await axios.get<ApiPost[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+  let score = 0;
+    const posts: UserPost[] = response.data
+      .filter((post: ApiPost) => {
+      for(let i = 0;i < post.body.length;i++){
+        if(post.body[i] === "/"){
+          score += 1;
+        }
+
+      }
+      score = 0;
+  })
+      .map((post: ApiPost): UserPost => ({
+        id: post.id,
+        body_counter: ,
+      }));
+
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+}
