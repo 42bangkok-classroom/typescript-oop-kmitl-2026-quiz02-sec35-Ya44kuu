0a3259c3c84e335d69b73b7d2ba21268cea31455
Ya44kuu <mr.sirisart1999@gmail.com>
2026-01-20 18:16:19 +0700

    test1

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -11,13 +11,14 @@ type commentPost ={
   title:string;
 }
 
-export async function safeFetchComment(commentId:number):Promise<commentPost>{
+export async function safeFetchComment(commentId:number):Promise<commentPost | null>{
   try{
     const res= await axios.get<api[]>("https://jsonplaceholder.typicode.com/comments");
-    if(commentId <0 || commentId >res.data.length || commentId === null || commentId === undefined){
+    if(commentId < 0 || commentId > res.data.length || !commentId) {
+      return null; // ส่งค่า null ออกไป
     }
     const output:commentPost[] = res.data.filter((post:api) => post.id === commentId);
-    console.log(output[0]);
+    // console.log(output[0]);
     return {
       id: output[0].id,
       title: output[0].title
@@ -26,6 +27,6 @@ export async function safeFetchComment(commentId:number):Promise<commentPost>{
     throw error;
   }
 }
-safeFetchComment(2);
+// safeFetchComment(2);
 
 
